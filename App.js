6291123d298c1fdffa0cc3f369f7cd91d735e2a3
duarte-arthur21 <ferreiraduarte21@gmail.com>
2023-03-26 11:13:01 -0300

    News project

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import './App.css';
+import {ImageBackground, Image, TouchableOpacity} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View >
+      <ImageBackground source={require('./space.avif')} resizeMode="cover" style={styles.background}>
+          <View>
+
+            <View style={styles.container}>
+                <a href="#" target="_blank">
+                  <Image source={require('./perfil.jpeg')} style={styles.perfil} />
+                </a>
+            </View>
+
+            <h1 className="title">Hello World!</h1>
+              <View className="card">
+        
+                <h3 className="title">
+                Olá eu sou o Arthur Duarte! 👋
+                </h3>
+
+              </View>
+              <p className="text">
+              Cursando sexto semestre de Tecnologia em Sistemas para Internet, Técnico de Informática,🧑‍💻!
+              </p>
+
+              <p className="text">
+              Tenho como objetivo entrar no mercado de trabalho como desenvolvedor Front-end
+              </p>
+
+              <p className="text">
+              Atual faço parte do time de programadores front-end em formação pela Plural Systems!
+              </p>
+          </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +42,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  background: {
+    flex: 1,
+    justifyContent: 'center',
+    height: '100%',
+    width: '100%',
+  },
+  perfil: {
+    width: 130,
+    height: 130,
+    borderRadius: 450,
+    padding: 1.5,
+    flex: 1,
+    position: 'relative',
+    alignItems: 'center'
+  }
 });
